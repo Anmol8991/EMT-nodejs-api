3f5d6176ed09e56790f57b14305c22d36f462e18
Anmol8991 <100134772+Anmol8991@users.noreply.github.com>
2023-09-01 12:14:44 -0400

    Update config.js

--- services/config.js
+++ services/config.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 // Setting up the Db connection
 const pool = mysql.createPool({
-    // host:  process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    socketPath: process.env.DB_PATH,
+    host:  process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    // socketPath: process.env.DB_PATH,
     user:  process.env.DB_USER,
     password: process.env.DB_PSWD,
     database: process.env.DB,
