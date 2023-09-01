c02d2647d8743dcd993ef1db4c07ad4dce6e3136
Anmol Arora <anmol.arora8991@gmail.com>
2023-09-01 12:08:10 -0400

    changes to config.js and .env-example to resolve deployment issue

--- services/config.js
+++ services/config.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql2");
 // Setting up the Db connection
 const pool = mysql.createPool({
-    host:  process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    // host:  process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    socketPath: process.env.DB_PATH,
     user:  process.env.DB_USER,
     password: process.env.DB_PSWD,
     database: process.env.DB,
@@ -15,7 +16,7 @@ pool.getConnection((err, conn) => {
   if(err) {
     console.log("Error connecting Db: ",err)
   }else{
-    console.log(" Database Connected successfully!: ", conn.config)
+    console.log(" Database Connected successfully!", conn.config)
   }
 })
 // now get a Promise wrapped instance of that pool
