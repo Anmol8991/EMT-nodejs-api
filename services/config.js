c38b18c62881f6150ceb758b162e928db9d4f465
Anmol8991 <100134772+Anmol8991@users.noreply.github.com>
2023-09-01 12:57:35 -0400

    Update config.js

--- services/config.js
+++ services/config.js
@@ -3,11 +3,9 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
     host:  process.env.DB_HOST,
     port: process.env.DB_PORT,
-    // socketPath: process.env.DB_PATH,
     user:  process.env.DB_USER,
     password: process.env.DB_PSWD,
     database: process.env.DB,
-    // connectTimeout: 20000,
     waitForConnections: true,
     connectionLimit: 10, // Maximum number of connections in the pool
     queueLimit: 0
