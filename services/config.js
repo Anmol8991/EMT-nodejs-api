441da8f54131b043654bf35c48152ca2ffe4c94e
Anmol8991 <100134772+Anmol8991@users.noreply.github.com>
2024-04-06 19:19:27 -0400

    Update config.js

--- services/config.js
+++ services/config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 // Setting up the Db connection
 const pool = mysql.createPool({
-    host:  process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host:  process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     user:  process.env.DB_USER,
-    password: process.env.DB_PSWD,
+    password: process.env.MYSQLPASSWORD,
     database: process.env.DB,
     waitForConnections: true,
     connectionLimit: 10, // Maximum number of connections in the pool
@@ -15,10 +15,10 @@ pool.getConnection((err, conn) => {
   if(err) {
     console.log("Error connecting Db: ",err)
     console.log(`${JSON.stringify({
-    host:  process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host:  process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     user:  process.env.DB_USER,
-    password: process.env.DB_PSWD,
+    password: process.env.MYSQLPASSWORD,
     database: process.env.DB,
     waitForConnections: true,
     connectionLimit: 10, // Maximum number of connections in the pool
